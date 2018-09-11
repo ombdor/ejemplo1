1dae1c74f77fe4500ed56c2bdb766cc27a17828f
omarbenj <43155372+omarbenj@users.noreply.github.com>
2018-09-10 19:45:32 -0500

    edicion de tirulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ejemplo sobre Laravel hola
+                    Ejemplo sobre 
                 </div>
 
                 
